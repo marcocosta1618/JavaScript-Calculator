8022cb3396b542e24a69cea5bc278f2254915f97
marcocosta1618 <marcocosta1618@gmail.com>
2021-09-01 17:31:52 +0200

    Fix typo
    Signed-off-by: marcocosta1618 <marcocosta1618@gmail.com>

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <p>Marco Costa 2021. The <a href="https://github.com/marcocosta1618/JavaScript-Calculator" target="_blank" rel="noreferrer">code of this app</a> is hosted at GitHub. 
-            Functional model of the <a href="https://xstate.js.org/viz/?gist=88e868a8d541927df43449a767fe3997" target="_blank" rel="noreferrer">calculator's state machine</a> on <a href="https://xstate.js.org/viz/" target="_blank" rel="noreferrer">Xstate Visualizer</a>.</p>
+            Functional model of the <a href="https://xstate.js.org/viz/?gist=88e868a8d541927df43449a767fe3997" target="_blank" rel="noreferrer">calculator's finite state machine</a> on <a href="https://xstate.js.org/viz/" target="_blank" rel="noreferrer">Xstate Visualizer</a>.</p>
         </footer>
     )
 }
