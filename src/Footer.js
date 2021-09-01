b2ecc35c83667ee95f65c28167f5eb67b258023f
marcocosta1618 <marcocosta1618@gmail.com>
2021-09-01 17:20:14 +0200

    Update calculator's state machine link and add link to Xstate Visualizer
    Signed-off-by: marcocosta1618 <marcocosta1618@gmail.com>

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <footer>
-            <p>Marco Costa 2021. The <a href="https://github.com/marcocosta1618/JavaScript-Calculator"
-                target="_blank" rel="noreferrer">code of this app</a> is hosted at GitHub.</p>
+            <p>Marco Costa 2021. The <a href="https://github.com/marcocosta1618/JavaScript-Calculator" target="_blank" rel="noreferrer">code of this app</a> is hosted at GitHub. 
+            Functional model of the <a href="https://xstate.js.org/viz/?gist=88e868a8d541927df43449a767fe3997" target="_blank" rel="noreferrer">calculator's state machine</a> on <a href="https://xstate.js.org/viz/" target="_blank" rel="noreferrer">Xstate Visualizer</a>.</p>
         </footer>
     )
 }
